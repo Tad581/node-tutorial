590f8616bce754c865706b45c47ce3cf0d1086cd
tadd582 <nguyendat58211@gmail.com>
2023-03-20 09:50:05 +0700

    topic: use scss & bootstrap

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
 // Template engine
 app.engine(".hbs", exphbs({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/trang-chu", (req, res) => res.render("home"));
 
