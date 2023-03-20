a2295c4bf4f5fbdfe7c069373f2f62c01b70dcb0
tadd582 <nguyendat58211@gmail.com>
2023-03-20 21:37:07 +0700

    refactor: refactor code with routes & controller folder

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-}));
+  })
+);
 app.use(express.json());
 
 // HTTP logger
@@ -21,15 +23,9 @@ app.engine(".hbs", exphbs({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/trang-chu", (req, res) => res.render("home"));
+const route = require("./routes");
 
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log("res.body :>> ", req.body);
-  res.render("search");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => console.log("http://localhost:" + port));
