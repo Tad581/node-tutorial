e1d948a249ba9bd3a3e8a258d8ee9f08cb6da975
tadd582 <nguyendat58211@gmail.com>
2023-03-21 14:27:05 +0700

    chore

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const siteRouter = require('./site');
 const newsRouter = require('./news');
 
 function route(app) {
-        app.use('/news', newsRouter);
+          app.use('/news', newsRouter);
         app.use('/', siteRouter);
 }
 
