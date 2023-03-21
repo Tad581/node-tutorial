6f073c3ddf579f41847d3ffda8269e6cc86c70db
tadd582 <nguyendat58211@gmail.com>
2023-03-21 14:13:46 +0700

    perf: add prettier, husky, lint-staged

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const exphbs = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
   express.urlencoded({
     extended: true,
-  })
+  }),
 );
 app.use(express.json());
 
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
-app.engine(".hbs", exphbs({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-const route = require("./routes");
+const route = require('./routes');
 
 // Routes init
 route(app);
 
-app.listen(port, () => console.log("http://localhost:" + port));
+app.listen(port, () => console.log('http://localhost:' + port));
