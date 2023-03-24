e7b064ca40ef13221730ad3a2d6e671f3af7e748
tadd582 <nguyendat58211@gmail.com>
2023-03-25 00:14:20 +0700

    feat: render detail course

---
+++ src/app/controllers/CoursesController.js
@@ -0,0 +1,16 @@
+const { mongooseToObject } = require('../../utils/mongoose');
+
+const Course = require('../models/Course');
+
+class CoursesController {
+    // [GET] /courses/:slug
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then((course) => {
+                res.render('show', { course: mongooseToObject(course) });
+            })
+            .catch(next);
+    }
+}
+
+module.exports = new CoursesController();
