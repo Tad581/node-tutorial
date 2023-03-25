d194d630dca30da743548978ca5148eedde4c5f1
tadd582 <nguyendat58211@gmail.com>
2023-03-25 15:28:56 +0700

    feat: create courses

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -7,9 +7,26 @@ class CoursesController {
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
             .then((course) => {
-                res.render('show', { course: mongooseToObject(course) });
+                res.render('courses/show', {
+                    course: mongooseToObject(course),
+                });
             })
             .catch(next);
+    }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        const course = new Course(req.body);
+        course
+            .save()
+            .then(() =>
+                res.redirect('/courses/' + req.body.name.toLowerCase()),
+            );
     }
 }
 
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const coursesController = require('../app/controllers/CoursesController');
 
+router.post('/store', coursesController.store);
+router.get('/create', coursesController.create);
 router.get('/:slug', coursesController.show);
 
 module.exports = router;
